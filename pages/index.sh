a8c44cbdbe77b8f44d16c1335643645707fdbfe9
Audrey Gu <audreymgu@gmail.com>
2026-06-12 21:58:17 -0400

    working basic leaderboard.

--- pages/index.sh
+++ pages/index.sh
@@ -16,10 +16,20 @@ done < data/images
 
 read CUR_ID OLD_TIME < data/current
 
+function returnLeaderboard {
+  while read -r id score; do
+    echo ${names[$id]} $score
+    # echo $score  
+
+  done < <(sort -k2,2nr data/scores)
+}
+
 htmx_page << EOF
   <h1>${PROJECT_NAME}</h1>
   <img src="${images[$CUR_ID]}"/>
   <p>${names[$CUR_ID]} is currently king-of-the-oauth.</p>
+  <h2>leaderboard</h2>
+  <pre>$(returnLeaderboard)</pre>
   <a href="${RECURSE_BASE_URL}/oauth/authorize?client_id=${APP_ID}&redirect_uri=${REDIRECT_URI}&response_type=code">Login</a>
 EOF
 
